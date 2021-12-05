cf10c3a535f6a42b6877860e9a48c00a7c327075
Polina Viro <polina.viro@gmail.com>
2021-12-05 10:41:30 -0500

    Cleanups for day 5
    - Refactor to remove trailing boolean
    - Consolidate frequency map and overlap counting

--- 2021/cmd/day5/main.go
+++ 2021/cmd/day5/main.go
@@ -55,8 +55,9 @@ func parseLine(raw string) (line, error) {
 	return line{from, to}, nil
 }
 
-// allPoints returns a slice of all points that are on the given line
-func (l line) allPoints(includeDiagonals bool) []point {
+// gridPoints returns a slice of all points that are on a horizontal or vertical line
+// If the line is diagonal, an empty slice is returned.
+func (l line) gridPoints() []point {
 	var ps []point
 
 	if l.from.x == l.to.x { // Horizontal
@@ -69,26 +70,37 @@ func (l line) allPoints(includeDiagonals bool) []point {
 		for i := min; i <= max; i++ {
 			ps = append(ps, point{i, l.from.y})
 		}
-	} else if includeDiagonals { // Diagonal
-		minX, maxX := minMax(l.from.x, l.to.x)
+	}
 
-		var top point
-		if minX == l.from.x {
-			top = l.from
-		} else {
-			top = l.to
-		}
-		// Determine if diagonal is going left or right from top to bottom
-		var yMod int64
-		if minY, _ := minMax(l.from.y, l.to.y); top.y == minY {
-			yMod = 1
-		} else {
-			yMod = -1
-		}
+	return ps
+}
 
-		for i, j := top.x, top.y; i <= maxX; i, j = i+1, j+yMod {
-			ps = append(ps, point{i, j})
-		}
+// diagonalPoints returns a slice of all points that are on a diagonal line
+// If the line is horizontal or vertical, an empty slice is returned.
+func (l line) diagonalPoints() []point {
+	if l.from.x == l.to.x || l.from.y == l.to.y {
+		return []point{}
+	}
+
+	minX, maxX := minMax(l.from.x, l.to.x)
+
+	var top point
+	if minX == l.from.x {
+		top = l.from
+	} else {
+		top = l.to
+	}
+	// Determine if diagonal is going left or right from top to bottom
+	var yDir int64
+	if minY, _ := minMax(l.from.y, l.to.y); top.y == minY {
+		yDir = 1
+	} else {
+		yDir = -1
+	}
+
+	var ps []point
+	for i, j := top.x, top.y; i <= maxX; i, j = i+1, j+yDir {
+		ps = append(ps, point{i, j})
 	}
 
 	return ps
@@ -114,27 +126,37 @@ func extractLines(fileName string) ([]line, error) {
 	return ls, s.Err()
 }
 
-// countPoints counts the frequency of each point's presence in the input lines
-func countPoints(lines []line, includeDiagonals bool) map[point]int64 {
-	m := make(map[point]int64)
-	for _, l := range lines {
-		ps := l.allPoints(includeDiagonals)
-		for _, p := range ps {
-			m[p] += 1
-		}
-	}
-	return m
-}
-
 // countOverlaps returns the number of points that have overlaps (more than 1 use)
-func countOverlaps(uses map[point]int64) int {
-	c := 0
-	for _, v := range uses {
-		if v >= 2 {
-			c += 1
-		}
+func countOverlaps(points []point) int {
+	m := make(map[point]int64)
+    c := 0
+	for _, p := range points {
+		m[p] += 1
+        if m[p] == 2 {
+          c += 1
+        }
 	}
 	return c
+}
+
+// countGridLineOverlaps returns the number of points that have overlapping grid lines
+// This excludes diagonal lines.
+func countGridLineOverlaps(lines []line) int {
+	var pts []point
+	for _, l := range lines {
+		pts = append(pts, l.gridPoints()...)
+	}
+	return countOverlaps(pts)
+}
+
+// countAllOverlaps returns the number of points that have overlapping lines
+func countAllOverlaps(lines []line) int {
+	var pts []point
+	for _, l := range lines {
+		pts = append(pts, l.gridPoints()...)
+		pts = append(pts, l.diagonalPoints()...)
+	}
+	return countOverlaps(pts)
 }
 
 func main() {
@@ -143,8 +165,6 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Input ingestion went wrong: ", err)
 		os.Exit(1)
 	}
-	nonDiagonalPoints := countPoints(ls, false)
-	allPoints := countPoints(ls, true)
-	fmt.Println("Part 1:", countOverlaps(nonDiagonalPoints))
-	fmt.Println("Part 2:", countOverlaps(allPoints))
+	fmt.Println("Part 1:", countGridLineOverlaps(ls))
+	fmt.Println("Part 2:", countAllOverlaps(ls))
 }
