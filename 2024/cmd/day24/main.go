b2dc45e2f04cac02047e2a363faee70d94dbbc2b
Polina Viro <polina.viro@gmail.com>
2024-12-25 18:53:05 +0200

    Add day 24, part 2.

--- 2024/cmd/day24/main.go
+++ 2024/cmd/day24/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"os"
+	"slices"
+	"sort"
 	"strings"
 	"time"
 
@@ -83,11 +86,197 @@ func computeZ(wires map[string]int) int {
 	return res
 }
 
+func setWires(x, y int) map[string]int {
+	m := map[string]int{}
+	for i := 0; i < 45; i++ {
+		k := fmt.Sprintf("x%02d", i)
+		b := x & 1
+		m[k] = b
+		x = x >> 1
+	}
+	for i := 0; i < 45; i++ {
+		k := fmt.Sprintf("y%02d", i)
+		b := y & 1
+		m[k] = b
+		y = y >> 1
+	}
+	return m
+}
+
+func countMistakes(rules []rule) int {
+	m := 0
+	for i := 0; i < 45; i++ {
+		w0 := setWires(0, 1<<i)
+		populateRules(w0, rules)
+		if computeZ(w0) != 1<<i {
+			m++
+		}
+
+		w1 := setWires(1<<i, 0)
+		populateRules(w1, rules)
+		if computeZ(w1) != 1<<i {
+			m++
+		}
+
+		w2 := setWires(1<<i, 1<<i)
+		populateRules(w2, rules)
+		if computeZ(w2) != (1<<i + 1<<i) {
+			m++
+		}
+	}
+	return m
+}
+
+func swapRules(orig []rule, a, b int) []rule {
+	rs := []rule{}
+	for i := 0; i < len(orig); i++ {
+		if i != a && i != b {
+			rs = append(rs, orig[i])
+		}
+		if i == a {
+			rs = append(rs, rule{
+				in1: orig[a].in1, in2: orig[a].in2,
+				op:  orig[a].op,
+				out: orig[b].out,
+			})
+		}
+		if i == b {
+			rs = append(rs, rule{
+				in1: orig[b].in1, in2: orig[b].in2,
+				op:  orig[b].op,
+				out: orig[a].out,
+			})
+		}
+	}
+	return rs
+}
+
+type swap struct {
+	a, b     int
+	mistakes int
+}
+
+// Return the swaps that return 0 mistakes for 0+2^i, 2^i+0, 2^i+2^i
+func potentialSwaps(rules []rule) [][4]swap {
+	baseline := countMistakes(rules)
+	fmt.Printf("Baseline mistakes: %d\n", baseline)
+	better := []swap{}
+	for i := 0; i < len(rules); i++ {
+		for j := i + 1; j < len(rules); j++ {
+			m := countMistakes(swapRules(rules, i, j))
+			if m+2 < baseline {
+				better = append(better, swap{
+					a:        i,
+					b:        j,
+					mistakes: m,
+				})
+			}
+		}
+	}
+	slices.SortFunc(better, func(a, b swap) int {
+		return a.mistakes - b.mistakes
+	})
+	for _, b := range better {
+		fmt.Printf("Swapping %s, %s: %d mistakes\n", rules[b.a].out, rules[b.b].out, b.mistakes)
+	}
+
+	sws := [][4]swap{}
+	for i := 0; i < len(better); i++ {
+		for j := i + 1; j < len(better); j++ {
+			for k := j + 1; k < len(better); k++ {
+				for l := k + 1; l < len(better); l++ {
+					s1 := better[i]
+					s2 := better[j]
+					s3 := better[k]
+					s4 := better[l]
+					gs := map[int]struct{}{}
+					gs[s1.a] = struct{}{}
+					gs[s1.b] = struct{}{}
+					gs[s2.a] = struct{}{}
+					gs[s2.b] = struct{}{}
+					gs[s3.a] = struct{}{}
+					gs[s3.b] = struct{}{}
+					gs[s4.a] = struct{}{}
+					gs[s4.b] = struct{}{}
+					if len(gs) < 8 {
+						continue
+					}
+					m := countMistakes(
+						swapRules(
+							swapRules(
+								swapRules(
+									swapRules(rules, s1.a, s1.b),
+									s2.a, s2.b),
+								s3.a, s3.b),
+							s4.a, s4.b))
+					if m == 0 {
+						sws = append(sws, [4]swap{s1, s2, s3, s4})
+						fmt.Printf("Swapping {%d, %d}, {%d, %d}, {%d, %d}, {%d, %d}: %d mistakes\n",
+							s1.a, s1.b, s2.a, s2.b, s3.a, s3.b, s4.a, s4.b, m)
+					}
+				}
+			}
+		}
+	}
+	fmt.Println("Finished finding all worthy swaps.")
+	return sws
+}
+
+func goodSwap(rules []rule, swaps [][4]swap) [4]swap {
+	bad := []int{}
+S:
+	for {
+		x := int(rand.Float64() * math.Pow(2, float64(40)))
+		y := int(rand.Float64() * math.Pow(2, float64(40)))
+		want := x + y
+		for i, s := range swaps {
+			if slices.Contains(bad, i) {
+				continue
+			}
+			nr := swapRules(
+				swapRules(
+					swapRules(
+						swapRules(rules, s[0].a, s[0].b),
+						s[1].a, s[1].b),
+					s[2].a, s[2].b),
+				s[3].a, s[3].b)
+
+			w := setWires(x, y)
+			populateRules(w, nr)
+			if computeZ(w) != want {
+				bad = append(bad, i)
+			}
+		}
+		if len(bad) == len(swaps)-1 {
+			break S
+		}
+	}
+	var g [4]swap
+	for i := 0; i < len(swaps); i++ {
+		if !slices.Contains(bad, i) {
+			g = swaps[i]
+		}
+	}
+	return g
+}
+
+func part2(rules []rule) string {
+	ps := potentialSwaps(rules)
+	ss := goodSwap(rules, ps)
+	gates := []string{}
+	for _, s := range ss {
+		gates = append(gates, rules[s.a].out)
+		gates = append(gates, rules[s.b].out)
+	}
+	sort.Strings(gates)
+	return strings.Join(gates, ",")
+}
+
 func main() {
 	t := time.Now()
 	wires, rules := extractInput(os.Args[1])
 	populateRules(wires, rules)
 	fmt.Printf("Part 1: %d\n", computeZ(wires))
-	fmt.Printf("Part2: %d\n", 0)
+	fmt.Printf("Part 2: %s\n", part2(rules))
 	fmt.Printf("Time elapsed: %s\n", time.Since(t))
 }
